682d0ece722eb50a6eef86cf9e6b5a80e83a930c
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-08-07 17:07:18 -0300

    player buttons

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -7,47 +7,43 @@
     prominent
   >
    <v-container>
-      <v-row dense>
-        <v-col cols="12">
-          <v-card
-            color="#385F73"
-            dark
-          >
-            <v-card-title class="headline">Unlimited music now</v-card-title>
-
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
-
-            <v-card-actions>
-              <v-btn text>Listen Now</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-
+      <v-row dense>       
         <v-col
-          v-for="(item, i) in items"
+          v-for="(song, i) in songs"
           :key="i"
           cols="12"
         >
           <v-card
-            :color="item.color"
+            :color="song.color"
             dark
           >
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
                 <v-card-title
                   class="headline"
-                  v-text="item.title"
+                  v-text="song.title"
                 ></v-card-title>
 
-                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                <v-card-subtitle v-text="song.artist"></v-card-subtitle>
+                <v-btn style="margin: 8px;" v-if="!isPlaying" @click="play">
+                <v-icon>mdi-play</v-icon>
+                </v-btn>
+                <v-btn style="margin: 8px;" v-else @click="pause">
+                <v-icon>mdi-pause</v-icon>
+                </v-btn>
+                <v-btn style="margin: 8px;">
+                <v-icon>mdi-stop</v-icon>
+                </v-btn>
               </div>
+
+             
 
               <v-avatar
                 class="ma-3"
                 size="125"
                 tile
               >
-                <v-img :src="item.src"></v-img>
+                <v-img :src="song.cape"></v-img>
               </v-avatar>
             </div>
           </v-card>
@@ -58,23 +54,68 @@
 </template>
 
 <script>
+import heroes from '../assets/capes/heroes.png'
+import tempic from '../assets/capes/tempic.png'
+import ivan from '../assets/capes/ivan.png'
+
 
 export default {
-    data: () => ({
-      items: [
+    data () {
+      return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      songs: [
         {
           color: '#1F7087',
-          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-          title: 'Supermodel',
-          artist: 'Foster the People',
+          cape: heroes,
+          title: 'Mythical and Mighty',
+          artist: 'Secession Studios ft Greg Dombrowski',
+          src: require('../assets/music/MythicalAndMighty.mp3'),
         },
         {
           color: '#952175',
-          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-          title: 'Halcyon Days',
-          artist: 'Ellie Goulding',
+          cape: tempic,
+          title: 'T E M P I C',
+          artist: 'Alan Lennon',
+        },
+        {
+          color: '#952175',
+          cape: ivan,
+          title: 'Facing Fears',
+          artist: 'Ivan Torrent',
+        },
+        {
+          color: '#952175',
+          cape: tempic,
+          title: 'T E M P I C',
+          artist: 'Alan Lennon',
         },
       ],
-    }),
+      player: new Audio()
+    }},
+    methods: {
+      play (song) {
+        if (typeof song.src != "undefined") {
+          this.current = song;       
+          
+
+          this.player.src = this.current.src
+        }
+          this.player.play();
+          this.isPlaying = true;
+      },
+      pause () {
+        this.player.pause()
+        this.isPlaying = false;
+      }
+    },
+    created () {
+      this.current = this.song[this.index]
+      this.player.src = this.current.src
+      this.player.play();
+      
+    }
   }
+  
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,4 +7,7 @@ export default new Vuetify({
     theme: {
         dark: true,
       },
+    icons: {
+      iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  },  
 });
